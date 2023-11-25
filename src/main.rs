ffc93c206e9f26d14082c6bc93aece354e927edf
Samson Christopher <91219719+Maghwyn@users.noreply.github.com>
2023-11-25 13:21:31 +0100

    feat(log): Implement simple logger and log for the app, and set the minimum leve to debug

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 extern crate serde_derive;
 
-use actix_web::{App, HttpServer};
+use actix_web::{App, HttpServer, middleware::Logger};
+use simple_logger::SimpleLogger;
 
 mod config;
 mod cors;
@@ -9,6 +10,9 @@ mod routes;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+	SimpleLogger::new().init().unwrap();
+	log::set_max_level(log::LevelFilter::Debug);
+
 	let conf = config::get();
 	let binding_address = format!("[::1]:{}", &conf.app.port);
 
@@ -16,6 +20,7 @@ async fn main() -> std::io::Result<()> {
 
 	HttpServer::new(move || {
 		App::new()
+			.wrap(Logger::new("%bb %Ts %s %r").log_target("http_log"))
 			.wrap(cors::options_delegate(conf.app.whitelist.clone()))
 			.service(routes::module::controller())
 	})
