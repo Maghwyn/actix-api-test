237d2bfcc4bd47b3a1ab155ce2f2c84f1a2d18b7
Samson Christopher <91219719+Maghwyn@users.noreply.github.com>
2023-11-25 13:20:10 +0100

    feat(main): Register the routes to the actix web app

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,25 @@
 extern crate serde_derive;
 
-use actix_web::{get, App, HttpResponse, HttpServer, Responder};
+use actix_web::{App, HttpServer};
 
 mod config;
 mod cors;
 mod errors;
-
-use errors::http_errors::ServiceError;
-
-#[get("/")]
-async fn hello() -> impl Responder {
-	HttpResponse::Ok().body("Hello world!")
-}
-
-#[get("/test")]
-async fn echo() -> Result<impl Responder, ServiceError> {
-	if true {
-		return Err::<HttpResponse, ServiceError>(ServiceError::Forbidden("Some error message".to_string()))
-	}
-
-	Ok(HttpResponse::Ok().body("Hey there!"))
-}
+mod routes;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
 	let conf = config::get();
 	let binding_address = format!("[::1]:{}", &conf.app.port);
 
-	HttpServer::new(|| {
+	// log::info!("Server running on http://{}", binding_address);
+
+	HttpServer::new(move || {
 		App::new()
 			.wrap(cors::options_delegate(conf.app.whitelist.clone()))
-			.service(hello)
-			.service(echo)
+			.service(routes::module::controller())
 	})
-	.bind(binding_address)?
+	.bind(binding_address.clone())?
 	.run()
 	.await
 }
